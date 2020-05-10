535ce9abc21676035b55beb5ac7eb3df560954c8
Kevin Boosten <kboosten@ilionx.com>
2020-05-10 20:57:46 +0200

    ci: add AppCenter post clone scripts

---
+++ ios/App/appcenter-post-clone.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# fail if any command fails
+set -e
+# debug log
+set -x
+
+# Required nodeJS version
+NODE_VERSION=10.17.0
+
+# workaround to override the v8 alias
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+nvm alias node10 "$NODE_VERSION"
+
+# go to root of project
+cd ../..
+
+# install dependencies
+npm i
+
+# run optimized production build
+npm run build -- --prod
