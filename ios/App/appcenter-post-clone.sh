c9ba882eb034457d82e875f490f60fe71019dccf
Kevin Boosten <kboosten@ilionx.com>
2020-05-10 21:53:33 +0200

    ci: update appcenter script with capacitor command

--- ios/App/appcenter-post-clone.sh
+++ ios/App/appcenter-post-clone.sh
@@ -22,3 +22,6 @@ npm i
 
 # run optimized production build
 npm run build -- --prod
+
+# copy the web assets to the native projects and updates the native plugins and dependencies based in package.json
+npx cap sync
